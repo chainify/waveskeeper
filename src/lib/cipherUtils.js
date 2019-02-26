2848588a9a76666ab2c37381c9676d4bde430ae7
kmadorin <kmadorin@kmadorins-MacBook-Air.local>
2019-02-26 18:11:25 +0300

    SHA 256 hash for shared key added, default iv for AES added

--- src/lib/cipherUtils.js
+++ src/lib/cipherUtils.js
@@ -2,16 +2,26 @@ import CryptoJS from 'crypto-js';
 import base58 from './base58';
 import axlsign from './axlsign';
 
+const iv = CryptoJS.enc.Hex.parse("101112131415161718191a1b1c1d1e1f");
+
+function byteArrayToWordArrayEx(arr) {
+  const len = arr.length
+  const words = []
+  for (let i = 0; i < len; i++) {
+    words[i >>> 2] |= (arr[i] & 0xff) << (24 - (i % 4) * 8)
+  }
+  return CryptoJS.lib.WordArray.create(words)
+}
 
 function getSharedKey(secretKey, publicKey) {
-	return base58.encode(axlsign.sharedKey(base58.decode(secretKey), base58.decode(publicKey)));
+	return CryptoJS.SHA256(byteArrayToWordArrayEx(axlsign.sharedKey(base58.decode(secretKey), base58.decode(publicKey))));
 }
 
 export function encryptMessage(message, privateKey, publicKey) {
-	return CryptoJS.AES.encrypt(message, getSharedKey(privateKey, publicKey)).toString();
+	return CryptoJS.AES.encrypt(message, getSharedKey(privateKey, publicKey), {iv: iv}).toString();
 }
 
 export function decryptMessage(message, privateKey, publicKey) {
-	const bytes = CryptoJS.AES.decrypt(message, getSharedKey(privateKey, publicKey));
+	const bytes = CryptoJS.AES.decrypt(message, getSharedKey(privateKey, publicKey), {iv: iv});
 	return bytes.toString(CryptoJS.enc.Utf8);
 }
